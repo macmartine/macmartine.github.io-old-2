94f9dd460b4a014d5d032d16cab0358ec652786e
Mac Martine <mac.martine@gmail.com>
2023-02-17 09:48:31 +0100

    Upgrades

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,5 @@
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 
-
-const UpgradeHelper = require("@11ty/eleventy-upgrade-help");
-
-module.exports = function(eleventyConfig) {
-  // If you have other `addPlugin` calls, it’s important that UpgradeHelper is added last.
-  eleventyConfig.addPlugin(UpgradeHelper);
-};
-
 module.exports = function(eleventyConfig) {
 
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
